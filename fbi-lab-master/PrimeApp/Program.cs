3a72b43d113c9e551dfc1caa5e4b59347a07784b
katyakarpocheva <43835894+katyakarpocheva@users.noreply.github.com>
2018-11-29 10:56:56 +0700

    Update Program.cs

--- fbi-lab-master/PrimeApp/Program.cs
+++ fbi-lab-master/PrimeApp/Program.cs
@@ -10,7 +10,7 @@ namespace PrimeApp
 
             var srv = new Prime.Services.PrimeService();
 
-            var prim = srv.PrimeName("Katya");
+            var prim = srv.PrimeName("Karpocheva");
 
             Console.WriteLine($"PrimeName: {prim}");
 
